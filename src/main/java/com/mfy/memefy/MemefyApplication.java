24c21157b695aa556f0a2c9c46dfad09000c500a
Oleh <iivasiukoleh@gmail.com>
2025-04-16 19:55:12 +0300

    init commit

---
+++ src/main/java/com/mfy/memefy/MemefyApplication.java
@@ -0,0 +1,18 @@
+package com.mfy.memefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The {@link MemefyApplication} class
+ *
+ * @author Oleh Ivasiuk
+ */
+@SpringBootApplication
+public class MemefyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemefyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfy/memefy/MemefyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfy.memefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemefyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
