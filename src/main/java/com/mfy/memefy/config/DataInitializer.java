d9522e10f97bf0910d2090d587a5a57de997cfa8
Oleh <iivasiukoleh@gmail.com>
2025-04-19 17:18:34 +0300

    [server]: fix: removed lazy-initialization.

--- src/main/java/com/mfy/memefy/config/DataInitializer.java
+++ src/main/java/com/mfy/memefy/config/DataInitializer.java
@@ -17,25 +17,25 @@ import java.util.Random;
  *
  * @author Oleh Ivasiuk
  */
-@Component
-public class DataInitializer implements CommandLineRunner {
-    private final MemeRepository memeRepository;
-    private final Random random = new Random();
-
-    public DataInitializer(MemeRepository memeRepository) {
-        this.memeRepository = memeRepository;
-    }
-
-    @Override
-    public void run(String... args) {
-        if (memeRepository.count() == 0) {
-            for (long i = 1; i <= 10; i++) {
-                MemeEntity meme = new MemeEntity();
-                meme.setName("Meme " + i);
-                meme.setImageUrl("https://example.com/meme" + i + ".jpg");
-                meme.setLikes(random.nextLong(100));
-                memeRepository.save(meme);
-            }
-        }
-    }
-}
+//@Component
+//public class DataInitializer implements CommandLineRunner {
+//    private final MemeRepository memeRepository;
+//    private final Random random = new Random();
+//
+//    public DataInitializer(MemeRepository memeRepository) {
+//        this.memeRepository = memeRepository;
+//    }
+//
+//    @Override
+//    public void run(String... args) {
+//        if (memeRepository.count() == 0) {
+//            for (long i = 1; i <= 10; i++) {
+//                MemeEntity meme = new MemeEntity();
+//                meme.setName("Meme " + i);
+//                meme.setImageUrl("https://example.com/meme" + i + ".jpg");
+//                meme.setLikes(random.nextLong(100));
+//                memeRepository.save(meme);
+//            }
+//        }
+//    }
+//}
